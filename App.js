5f73883917ccb2ce4a70b42165a717baf70771a3
dickymahfudin <dickymahfudin13@gmail.com>
2020-09-16 19:25:09 +0700

    navigation

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 import WebView from 'react-native-webview';
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-const App = () => {
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
   return <WebView source={{uri: 'https://reactnative.dev/'}} />;
-};
+}
 
-export default App;
+const Tab = createBottomTabNavigator();
 
-const styles = StyleSheet.create({});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
